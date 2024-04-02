53afe763ff25f9507ad89e44773a0c3aa12bc5cf
jainl28patel <jainilpatel2003@gmail.com>
2024-04-03 05:15:44 +0530

    add Node and Round Robin

--- master-node.py
+++ master-node.py
@@ -4,7 +4,54 @@ import yaml
 from pathlib import Path
 import json 
 
-NODES = []
+class Node:
+    def __init__(self) -> None:
+        self.node = []
+        self.lock = threading.Lock()
+
+    def __str__(self) -> str:
+        with self.lock:
+            return json.dumps(self.node, indent=4)
+
+    def add(self, node):
+        with self.lock:
+            self.node.append(node)
+    
+    def get_task_node_ip(self, task):
+        with self.lock:
+            ret = []
+            for node in self.node:
+                name = next(iter(node))
+                if task in node[name]["task"]:
+                    ret.append(node[name]["ip"])
+            return ret
+        
+    def get_task_list(self):
+        with self.lock:
+            ret = set()
+            for node in self.node:
+                name = next(iter(node))
+                ret.add(node[name]["task"])
+            return ret
+
+class RoundRobin:
+    def __init__(self, node_list: Node) -> None:
+        self.task_to_ip = {}
+        self.last_task_ip = {}
+
+        for task in node_list.get_task_list():
+            self.task_to_ip[task] = []
+            for ip in node_list.get_task_node_ip(task):
+                self.task_to_ip[task].append(ip)
+            self.last_task_ip[task] = 0
+
+        self.lock = threading.Lock()
+
+    def get_ip(self, task):
+        with self.lock:
+            ip = self.task_to_ip[task][self.last_task_ip[task]]
+            self.last_task_ip[task] = (self.last_task_ip[task] + 1) % len(self.task_to_ip[task])
+            return ip
 
 def load_config():
     # load yaml
@@ -15,21 +62,16 @@ def load_config():
     with open('config.yaml') as file:
         config = yaml.full_load(file)
         for node in config["nodes"]:
-            NODES.append(node)
-        print(NODES)
+            NODE.add(node)
 
 def handle_request(data):
-    # Parse data and send to respective node
-    # result = '{"req_id":1, "task": "t1"}'
-    result = json.loads(data)
-    # print(result["task"])
-    
+    pass
     
 
 def server():
     # Listen for incoming headers and send to respective nodes or stores in queue
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('localhost', 5000))    # later change to master node IP as per yaml file
+    server.bind(('localhost', 8000))    # later change to master node IP as per yaml file
     server.listen(5)
 
     while True:
@@ -43,7 +85,10 @@ def heartbeat():
 
 
 def main():
+    global NODE, RR
+    NODE = Node()
     load_config()
+    RR = RoundRobin(NODE)
     
     server_thread = threading.Thread(target=server)
     heartbeat_thread = threading.Thread(target=heartbeat)
@@ -52,7 +97,12 @@ def main():
     heartbeat_thread.start()
 
     server_thread.join()
-    heartbeat_thread.join()    
+    heartbeat_thread.join()
+
+
+# Global variables
+NODE = None
+RR = None
 
 if __name__ == "__main__":
     main()
