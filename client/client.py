5af58765ad5f8e357b3cbd3773a8af10bc2fd47c
0xn4utilus <gyanendrabanjare8@gmail.com>
2024-04-04 19:47:13 +0530

    update client

--- client/client.py
+++ client/client.py
@@ -3,9 +3,14 @@ import json
 import asyncio
 from flask import Flask, request, jsonify
 import uuid
+from pathlib import Path
+import yaml
 
 app = Flask(__name__)
 
+
+NODES_URL=[]
+MASTER_URL = ""
 
 def request_parser(request):
     if isinstance(request, dict):
@@ -35,7 +40,7 @@ async def send_data_to_socket(url, data):
 
 @app.route("/", methods=["GET"])
 def user_get_request():
-    tasks = [send_data_to_socket(node_url, "GET") for node_url in node_urls]
+    tasks = [send_data_to_socket(node_url, "GET") for node_url in NODES_URL]
     node_responses = asyncio.run(asyncio.gather(*tasks))
     return jsonify([response.decode() for response in node_responses]), 200
 
@@ -45,7 +50,7 @@ async def user_post_request():
     request_data = request.get_json()
     task, task_id, search_text = request_parser(request_data)
     await send_data_to_socket(
-        master_url,
+        MASTER_URL,
         json.dumps({"task_id": task_id, "task": task}),
     )
     tasks = [
@@ -55,23 +60,43 @@ async def user_post_request():
                 json.dumps({"task_id": task_id, "data": {"search_text": search_text}}),
             )
         )
-        for node_url in node_urls
+        for node_url in NODES_URL
     ]
     for task in asyncio.as_completed(tasks):
         response = await task
         response_data = json.loads(response.decode())
-        if response_data.get("status") == "yes":
+        if response_data.get("status") == "200":
             for t in tasks:
                 t.cancel()  # cancel all other tasks
             return jsonify(response_data), 200
     return jsonify({"message": "Task cannot be performed"}), 500
 
 
-node_urls = ["localhost:8080"]  # "localhost:8082", "localhost:8083", "localhost:8084"]
-master_url = "localhost:8000"
+def load_config():
+    # load yaml
+    global NODES_IP,MASTER_IP
+    config_path = Path("config.yaml")
+    if not config_path.exists():
+        print("config.yaml not found.") 
+        return 0
+    with open('config.yaml') as file:
+        config = yaml.full_load(file)
+        
+        MASTER_URL = f"{config["master"]["ip"]}:{config["master"]["port"]}" 
+        
+        for node in config["nodes"]:
+            name = next(iter(node))
+            node_ip = node[name]["ip"]    
+            node_port = node[name]["port"]    
+            NODES_URL.append(f"{node_ip}:{node_port}")
+        
+    return 1
 
 if __name__ == "__main__":
-    app.run(port=80)
+    if not load_config():
+        print("error in loading config.")
+    else:
+        app.run(port=8045)
 
 
 # {
