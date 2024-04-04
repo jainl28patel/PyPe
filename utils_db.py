cc581eb518881915cc5423148a580ebf8b8e792a
Jainil Patel <95585633+jainl28patel@users.noreply.github.com>
2024-04-04 18:03:31 +0530

    Adds vector db

---
+++ utils_db.py
@@ -0,0 +1,19 @@
+import weaviate
+import sys
+
+URL = ""
+APIKEY = ""
+
+def connect():
+
+    client = weaviate.connect_to_wcs(
+        cluster_url=URL,
+        auth_credentials=weaviate.auth.AuthApiKey(APIKEY))
+
+    if client.is_live():
+        print("CONNECTED TO WEAVIATE DATABASE")
+    else:
+        print("UNABLE TO CONNECT TO WEAVIATE DATABASE")
+        sys.exit()
+
+    return client
